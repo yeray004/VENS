3791d44345eae2cf6f2b799d48167a15aaf44cd4
Yeray Guachetá Carreño <yeye20041@gmail.com>
2023-12-01 06:53:05 -0500

    Nueva rama

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import SideMenu from './components/sideMenu.jsx'
 import Home from './views/home/Home.jsx'
 import Button from './components/Button.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import DesignProyects from './views/design/DesignProyects.jsx'
-
+import Art from './views/art/Art.jsx'
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
     <SideMenu/>
     <Home/>
     <DesignProyects/>
+    <Art/>
     <Button/>
   </>
   )
--- src/views/art/Art.jsx
+++ src/views/art/Art.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Art() {
   return (
-    <div>Art</div>
+    <article className='sideMenuSpace'>
+      <section>
+        <h3>Digitales</h3>
+        <hr />
+        <div>
+          <img src="" alt="Dibujo" />
+          <img src="" alt="Dibujo" />
+          <img src="" alt="Dibujo" />
+          <img src="" alt="Dibujo" />
+        </div>
+      </section>
+      <section>
+        <h3>Analogas</h3>
+        <hr />
+      </section>
+    </article>
   )
 }
 
