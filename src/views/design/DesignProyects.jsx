8e90288aa700f2e87a5888a6fa7e5482573ba580
Yeray Guachetá Carreño <yeye20041@gmail.com>
2023-12-01 12:51:25 -0500

    seccción personajes

--- src/views/design/DesignProyects.jsx
+++ src/views/design/DesignProyects.jsx
@@ -11,6 +11,10 @@ import themysciraTarot from '../../assets/PROYECTOS/ID-VISUAL/THEMYSCIRA.png'
 import blackTarot from '../../assets/PROYECTOS/ID-VISUAL/THEMYSCIRABLACK.png'
 import whiteTarot from '../../assets/PROYECTOS/ID-VISUAL/THEMYSCIRAWHITE.png'
 
+import fresa1 from '../../assets/PROYECTOS/PERSONAJES/FRESA1.png'
+import fresa2 from '../../assets/PROYECTOS/PERSONAJES/FRESA2.png'
+import maquina from '../../assets/PROYECTOS/PERSONAJES/MAQUINA.png'
+
 
 
 function DesignProyects() {
@@ -47,7 +51,27 @@ function DesignProyects() {
         <img src={blackTarot} alt="icono venus" />
         <img src={whiteTarot} alt="themyscira tarot" />
       </section>
+      <section className='proyectSection2'>
+        <p>Desde la identidad visual hasta las ilustraciones de su producto principal, un tarot ilustrado de mujeres contemporáneas y de la época, son realizados por mí, demuestra mi experiencia en el manejo de programas de dibujo y diseño.</p>
+        <div>
+          <a href="#">THEMYSCIRA</a>
+        </div>
+      </section>
       <h1 className='seccionTitulos'>Personajes</h1><hr />
+      <section className='proyectSection4'>
+        <div>
+          <img src={fresa1} alt="Personaje" />
+          <img src={fresa2} alt="Personaje" />
+          <p>👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀</p>
+        </div>
+        <div>
+          <img src={maquina} alt="Personaje" />
+          <h3>DISEÑO DE PERSONAJE - FRESA YOGUETA SUPER  SMASH</h3>
+          <p>Para la edición especial SUPER SMASH, diseñe uno de los personajes fusionando el sabor fresa de la yogueta con Kirby, el cual es el personaje de los videojuegos más reconocidos de SUPER SMASH.</p>
+          <h3>DISEÑO Y CREACIÓN DE PERSONAJE - MAQUINA SUPER  SMASH</h3>
+          <p>Un personaje más de los que diseñe para la edición especial fue esta máquina que era la representante de la línea gráfica que se creó, así mismo diseñe el cómo se vería en 3D para la elaboración de futuros displays.</p>
+        </div>
+      </section>
     </article>
   )
 }
